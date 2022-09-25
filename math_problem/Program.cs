02b6e466d922856669b858afd8b8426f6360d29e
Grandy005 <90681865+Grandy005@users.noreply.github.com>
2022-09-25 15:54:52 +0200

    Add files via upload

---
+++ math_problem/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math_problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int number = 0;
+            while (true)
+            {
+                Console.Write("Adj meg egy számot: ");
+                try
+                {
+                    number = Convert.ToInt32(Console.ReadLine());
+                    if (number > 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("0-nál nagyobb egész számot adj meg!");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Hibás adat!");
+                }
+            }
+            while (true)
+            {
+                if (number % 2 == 0)
+                {
+                    number = number / 2;
+                    Console.WriteLine(number);
+                    counter++;
+                }
+                else if (number == 1)
+                {
+                    break;
+                }
+                else
+                {
+                    number = (number * 3) + 1;
+                    Console.WriteLine(number);
+                    counter++;
+                }
+            }
+            Console.WriteLine($"A programnak {counter} lépésre volt szüksége hogy elérje az ismétlődést ");
+            Console.ReadKey();
+        }
+
+    }
+}
+
